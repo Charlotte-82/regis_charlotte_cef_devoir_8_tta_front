15d6e88d41164c6d6311fe76c5088ac5e23a4d2e
Charlotte-82 <charlotteregis1@gmail.com>
2025-05-26 20:06:22 +0200

    correction composant accueil

--- src/components/ArtisanTop.js
+++ src/components/ArtisanTop.js
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 
 function TopArtisans() {
   const { data: artisans, loading, error } = useFetch(getArtisansTop);
+  console.log("Artisans reçus dans le composant :", artisans);
 
   if (loading) return <p>Chargement...</p>;
   if (error) return <p>Erreur lors du chargement des artisans.</p>;
+  console.log("Artisans :", artisans);
 
   return (
     <div className="container mt-1">
       <div className="row">
-        {Array.isArray(artisans) &&
+        {Array.isArray(artisans) && artisans.length > 0 ? (
           artisans.map((a, index) => (
             <div className="col-md-4 d-flex divCard" key={index}>
               <div className="card mb-4 topCard">
@@ -31,7 +33,6 @@ function TopArtisans() {
                   <h4 className="card-subtitle mb-2 text-muted h4Text">
                     {a.specialite} — {a.ville}
                   </h4>
-
                   <p className="noteTop">
                     <strong>Note :</strong> {a.note}/5
                   </p>
@@ -45,8 +46,8 @@ function TopArtisans() {
                 </Link>
               </div>
             </div>
-          ))}
-        {artisans.length === 0 && (
+          ))
+        ) : (
           <p>Aucun artisan à afficher pour le moment.</p>
         )}
       </div>
